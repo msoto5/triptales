e3d5529e04963d05e203a388b68efe17c55534b1
Abhishek Shadakshari <2772079s@student.gla.ac.uk>
2024-03-07 20:44:25 +0000

    idk

--- triptales/admin.py
+++ triptales/admin.py
@@ -11,11 +11,9 @@ class locationAdmin(admin.ModelAdmin):
 class postAdmin(admin.ModelAdmin):
     list_display = ('text', 'author', 'likes')
 
-class commentAdmin(admin.ModelAdmin):
-    list_display = ('text')
 
 admin.site.register(Country, CountryAdmin)
 admin.site.register(Location, locationAdmin)
 admin.site.register(VacationPost, postAdmin)
-admin.site.register(Comment, commentAdmin)
+admin.site.register(Comment)
 admin.site.register(UserProfile)
