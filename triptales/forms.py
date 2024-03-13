b06dbb2a3eb82e8faf70db8e245ddbf3deae72a0
Abhishek Shadakshari <2772079s@student.gla.ac.uk>
2024-03-13 15:45:30 +0000

    forms

--- triptales/forms.py
+++ triptales/forms.py
@@ -1,6 +1,49 @@
 """Forms for the triptales app."""
 from django import forms
-from triptales.models import Country, Location, VacationPost
+from triptales.models import Country, Location, VacationPost, UserProfile, Comment
+from django.contrib.auth.models import User
+
+class CountryForm(forms.ModelForm):
+    name = forms.CharField(help_text="Please enter the name of the country.")
+    continent = forms.CharField(help_text="Please enter the continent of this country.")
+
+    class Meta:
+        model = Country
+        fields = ('name', 'continent')
+
+
+class LocationForm(forms.ModelForm):
+    name = forms.CharField(help_text="Please enter the name of the location.")
+    country = forms.ModelChoiceField(queryset=Country.objects)
+    vibe = forms.RadioSelect()
+    setting = forms.RadioSelect()
+    travelPartner = forms.RadioSelect()
+    climate = forms.RadioSelect()
+
+    class Meta:
+        model = Location
+        exclude = ('views', 'posts')
+
+class CommentForm(forms.ModelForm):
+    text = forms.CharField(help_text="")
+
+    class Meta:
+        model = Comment
+        fields = ('text',)
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+class UserProfileForm(forms.ModelForm):
+
+    class Meta:
+        model = UserProfile
+        fields = ('picture', 'bio')
+
 
 class VacationPostForm(forms.ModelForm):
     country = forms.ModelChoiceField(queryset=Country.objects.all(), empty_label="Select Country")
