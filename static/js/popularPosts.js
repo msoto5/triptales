0c71190b1cfa3363ce53e1d06e7970800068d8a7
CammyK16 <cammyk16@live.com>
2024-03-22 13:41:29 +0000

    Refactored out inline CSS and JS to induvidual files

---
+++ static/js/popularPosts.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    loadPosts('likes', '#top-posts-container');
+    loadPosts('recent', '#recent-posts-container');
+
+    function loadPosts(type, containerId) {
+        const xhttp = new XMLHttpRequest();
+        xhttp.onload = function () {
+            const response = JSON.parse(this.responseText);
+            const posts = response.posts;
+            let html = '';
+            posts.forEach(function (post) {
+                const createdAtDate = new Date(post.created_at);
+                const formattedCreatedAt = createdAtDate.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' });
+                html += generatePost(post, formattedCreatedAt);
+            });
+            $(containerId).html(html);
+        };
+        xhttp.open("GET", "/get_all_posts/" + type + "/", true);
+        xhttp.send();
+    }
+});
